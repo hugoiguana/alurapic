196303119d59588acc284bfa44b0f1273fa3c54c
hugoiguana <hugo.iguanaa@gmail.com>
2018-04-18 22:29:28 -0300

    Configuração inicial do projeto alurapic - Angular

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,8 @@
+angular.module('alurapic').controller('FotosController', function($scope) {
+
+    $scope.foto = {
+        titulo : 'Leão',
+        url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
+    };
+
+});
