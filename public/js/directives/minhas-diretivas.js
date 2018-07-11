097f11c790cd1bc0177e0bae66301673fcb8a37f
Hugo Fernando da Mota <hugo.mota@mvrec.local>
2018-07-11 13:58:53 -0300

    Aula 09 - Tornando nosso cadastro ainda melhor!

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -10,18 +10,27 @@ angular.module('minhasDiretivas', []).directive('meuPainel', function(){
 		titulo: '@'
 	};
 	
-	ddo.templateUrl = 'js/directives/meu-painel.html';
-	
-	/*
-    ddo.template = 
-                '<div class="panel panel-default">'
-            +   '   <div class="panel-heading">'
-            +   '        <h3 class="panel-title text-center">{{titulo}}</h3> '
-            +   '   </div>'
-            +   '   <div class="panel-body" ng-transclude>'
-            +   '   </div>'
-            +   '</div>';	
-	*/
-	
+	ddo.templateUrl = 'js/directives/meu-painel.html';	
 	return ddo;	
+	
+}).directive('minhaFoto', function() {
+	var ddo = {};
+	ddo.restrict = "AE";
+	ddo.scope = {
+		titulo: '@',
+		url : '@'
+	}
+	ddo.template = '<img class="img-responsive center-block" src="{{url}}" alt="{{titulo}}">';
+
+	return ddo;
+}).directive('meuBotaoPerigo', function() {
+	var ddo = {};
+	ddo.restrict = "E";
+	ddo.scope = {
+		nome: '@',
+		acao : '&'
+	}
+	ddo.template = '<button class="btn btn-danger btn-block" ng-click="acao()">{{nome}}</button>';
+
+	return ddo;
 });
